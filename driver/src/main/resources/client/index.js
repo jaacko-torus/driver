fddbcd8c6cddb6a580d58980e2ac661ec207a825
jaacko-torus <jaacko.torus@gmail.com>
2022-06-15 22:04:55 -0500

    on document load

--- driver/src/main/resources/client/index.js
+++ driver/src/main/resources/client/index.js
@@ -35,47 +35,11 @@ const themes = {
 /** @type {keyof typeof themes} */
 let selected_theme = "dark"
 
-let username = window.prompt("what is your username");
-
-let socket = new WebSocket(`ws://localhost:9001/greeter?username=${username}`)
-
-const $messaging_data = document.getElementById("messaging_data")
-const $messaging_button = document.getElementById("messaging_button")
-const $chat_area = document.getElementById("chat_area")
-const $toggle_theme = document.getElementById("toggle_theme")
-const $root = document.querySelector(":root");
-
-document.addEventListener("DOMContentLoaded", (e) => {
-    // console.log(themes)
-    [selected_theme] = Object.entries(themes).find(([theme, props]) => props.matched)
-    // console.log(selected_theme)
-    // selected_theme = used_theme
-})
-
-/**
- * @param {string | null} data
- */
-const send_data = (data = null) => {
-    if (data !== null) {
-        let val = data.trim()
-        if (val === "") return
-        socket.send(val)
-        return
-    }
-
-    /** @type {string} */
-    const body = $messaging_data.value
-    $messaging_data.value = ""
-
-    let val = body.trim()
-    if (val === "") return
-    socket.send(val)
-}
-
 const change_theme = (selected_theme) => {
     const vars = themes[selected_theme].vars
     const set_property = ([name, value]) => $root.style.setProperty(name, value)
     Object.entries(vars).forEach(set_property)
+    console.log("themes changed")
 }
 
 const toggle_theme = () => {
@@ -91,25 +55,27 @@ const toggle_theme = () => {
     }
 }
 
-$toggle_theme.onclick = e => {
-    toggle_theme()
-    change_theme(selected_theme)
-}
+const $messaging_data = document.getElementById("messaging_data")
+const $messaging_button = document.getElementById("messaging_button")
+const $chat_area = document.getElementById("chat_area")
+const $toggle_theme = document.getElementById("toggle_theme")
+const $root = document.querySelector(":root");
 
-$messaging_button.onclick = e => {
-    send_data()
-    $messaging_button.blur()
-}
+/**
+ * @param {WebSocket} socket
+ */
+const send_data = (socket) => (data = "") => {
+    /** @type {string} */
+    const body = $messaging_data.value
+    $messaging_data.value = ""
 
-document.onkeydown = e => {
-    if (e.key === "Enter" && document.activeElement === $messaging_data) {
-        send_data()
-    }
-    if (e.key === "Escape") {
-        $messaging_data.blur()
-    }
-    return true;
-};
+    /** @type {string | undefined} */
+    const val = [data.trim(), body.trim()].find(e => e !== "")
+
+    if (val === undefined) return
+
+    socket.send(val)
+}
 
 /**
  * Spawns message in html
@@ -157,30 +123,58 @@ function on_message(username, timestamp, body) {
     MaterializeMessage(username, timestamp, body)
 }
 
-socket.onmessage = event => {
-    const message = JSON.parse(event.data);
-    const events = {
-        "message": data => on_message(data.username, data.timestamp, data.message),
-        "user_joined": data => on_message("[server]", data.timestamp, `${data.username} has joined`),
-        "user_left": data => on_message("[server]", data.timestamp, `${data.username} has left`),
+document.addEventListener("DOMContentLoaded", (e) => {
+    [selected_theme] = Object.entries(themes).find(([theme, props]) => props.matched)
+    change_theme(selected_theme)
+
+    let username = window.prompt("what is your username");
+    let socket = new WebSocket(`ws://localhost:9001/greeter?username=${username}`)
+
+    document.onkeydown = e => {
+        if (e.key === "Enter" && document.activeElement === $messaging_data) {
+            send_data(socket)()
+        }
+        if (e.key === "Escape") {
+            $messaging_data.blur()
+        }
+        return true;
+    };
+
+    $toggle_theme.onclick = e => {
+        toggle_theme()
+        change_theme(selected_theme)
     }
 
-    events[message.type](message.data)
-}
-
-socket.onopen = event => {
-    MaterializeMessage("[server]", dayjs().format("hh:mma"), "[open] Connection established")
-};
-
-socket.onclose = event => {
-    if (event.wasClean) {
-        MaterializeMessage("[server]", event.data.timestamp, `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`)
-    } else {
-        // server process killed or network down, event.code is *usually* 1006 in this case
-        MaterializeMessage("[server]", event.data.timestamp, "[close] Connection died")
+    $messaging_button.onclick = e => {
+        send_data(socket)()
+        $messaging_button.blur()
     }
-};
 
-socket.onerror = error => {
-    MaterializeMessage("[server]", error?.data?.timestamp ?? dayjs().format("hh:mma"), `[error] ${error.message}`)
-};
+    socket.onmessage = event => {
+        const message = JSON.parse(event.data);
+        const events = {
+            "message": data => on_message(data.username, data.timestamp, data.message),
+            "user_joined": data => on_message("[server]", data.timestamp, `${data.username} has joined`),
+            "user_left": data => on_message("[server]", data.timestamp, `${data.username} has left`),
+        }
+
+        events[message.type](message.data)
+    }
+
+    socket.onopen = event => {
+        MaterializeMessage("[server]", dayjs().format("hh:mma"), "[open] Connection established")
+    };
+
+    socket.onclose = event => {
+        if (event.wasClean) {
+            MaterializeMessage("[server]", event.data.timestamp, `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`)
+        } else {
+            // server process killed or network down, event.code is *usually* 1006 in this case
+            MaterializeMessage("[server]", event.data.timestamp, "[close] Connection died")
+        }
+    };
+
+    socket.onerror = error => {
+        MaterializeMessage("[server]", error?.data?.timestamp ?? dayjs().format("hh:mma"), `[error] ${error.message}`)
+    };
+})
